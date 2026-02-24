358e1bcc55bd13c10b52fb1c32c7f108e05cbf6f
Giuseppe Li Gioi <pepe2117k@gmail.com>
2026-02-24 13:17:36 +0100

    added. base responsive header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,3 +1,34 @@
+"use client";
+import styles from "./Header.module.css";
+import Container from "@/components/ui/Container/Container";
+import Logo from "@/components/ui/Logo/Logo";
+import { Mail, Phone, Menu, X } from "lucide-react";
+import { useState } from "react";
 export default function Header() {
-  return <h2>Sono Header</h2>;
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  return (
+    <header className={styles.containerHeader}>
+      <Container>
+        <div className={styles.containerInnerHeader}>
+          <Logo />
+          <div className={styles.contactsMobile}>
+            <a href="tel:3331234567" className={styles.contactA}>
+              <Phone />
+              <span className={styles.contactSpan}>333 1234567</span>
+            </a>
+            <a href="mailto:ligioimarco@gmail.com" className={styles.contactA}>
+              <Mail />
+              <span className={styles.contactSpan}>ligioimarco@gmail.com</span>
+            </a>
+          </div>
+          <button
+            className={styles.btnHamburgher}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <Menu />
+          </button>
+        </div>
+      </Container>
+    </header>
+  );
 }
