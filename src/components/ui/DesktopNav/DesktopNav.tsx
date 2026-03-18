23c57e96a9fd6e80eb7371a4612332730bc26522
Giuseppe Li Gioi <pepe2117k@gmail.com>
2026-03-18 11:36:03 +0100

    added. active link style

--- src/components/ui/DesktopNav/DesktopNav.tsx
+++ src/components/ui/DesktopNav/DesktopNav.tsx
@@ -1,12 +1,31 @@
+"use client";
 import { NavigationItem } from "@/data/navigation";
 import Container from "@/components/ui/Container/Container";
 import styles from "./DesktopNav.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ChevronDown } from "lucide-react";
 type DesktopNavProps = {
   items: NavigationItem[];
 };
+
 export default function DesktopNav({ items }: DesktopNavProps) {
+  const activeUrl = usePathname();
+
+  const handleLinkActive = (
+    id: string,
+    url?: string,
+    children?: { href?: string }[],
+  ): string => {
+    if (id === "catalogo") {
+      const isCatalogActive = children?.some(
+        (child) => child.href && child.href === activeUrl,
+      );
+      return isCatalogActive ? styles.active : "";
+    }
+    const isActive = url === activeUrl;
+    return isActive ? styles.active : "";
+  };
   return (
     <nav
       className={styles.navbarLg}
@@ -18,7 +37,7 @@ export default function DesktopNav({ items }: DesktopNavProps) {
             return (
               <Link
                 href={item.href}
-                className={styles.voiceNavLg}
+                className={`${styles.voiceNavLg} ${handleLinkActive(item.id, item.href)}`}
                 key={item.id}
               >
                 {item.label}
@@ -28,7 +47,10 @@ export default function DesktopNav({ items }: DesktopNavProps) {
           if (item.children && item.children.length > 0) {
             return (
               <div className={styles.navItem} key={item.id}>
-                <button type="button" className={styles.navTrigger}>
+                <button
+                  type="button"
+                  className={`${styles.navTrigger} ${handleLinkActive(item.id, item.href, item.children)}`}
+                >
                   <span>{item.label}</span>
                   <ChevronDown className={styles.triggerIcon} />
                 </button>
